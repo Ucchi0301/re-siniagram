be898819823f9506205457ada65ee2a32d0d981f
yyamauchi0301 <yuto.yamauchi@fabeee.co.jp>
2025-01-13 13:38:28 +0900

    グループに関連した投稿のみ取得

--- backend/api/controllers/post_controller.py
+++ backend/api/controllers/post_controller.py
@@ -1,18 +1,39 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from common.models import PostContent
 from api.serializers.post_serializer import PostSerializer
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
+from common.models import PostContent, GroupMembership, Group
 
+from ..permissions import IsInGroup
+
+
+# ユーザーが所属しているグループのユーザー一覧取得
+def get_group_users(user) -> list:
+    group_membership = GroupMembership.objects.filter(user=user).first()
+    group_id = group_membership.group.id
+    group_users = GroupMembership.objects.filter(group_id=group_id).values_list(
+        "user", flat=True
+    )
+    return group_users
+
+
+# 投稿一覧取得
 class PostListView(APIView):
-    permission_classes = [IsAuthenticated]
+
+    permission_classes = [IsInGroup]
 
     def get(self, request):
-        queryset = PostContent.objects.all()
+        group_users = get_group_users(request.user)
+        queryset = PostContent.objects.filter(created_by__in=group_users)
         serializer = PostSerializer(queryset, many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+# 投稿作成
+class PostCreateView(APIView):
+
+    permission_classes = [IsInGroup]
 
     def post(self, request):
         serializer = PostSerializer(data=request.data)
@@ -22,8 +43,10 @@ class PostListView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# 投稿詳細取得、更新、削除
 class PostDetailView(APIView):
-    permission_classes = [IsAuthenticated]
+
+    permission_classes = [IsInGroup]
 
     def get(self, request, pk):
         queryset = PostContent.objects.get(pk=pk)
@@ -31,7 +54,7 @@ class PostDetailView(APIView):
         return Response(status=status.HTTP_200_OK, data=serializer.data)
 
     def put(self, request, pk):
-        queryset = PostContent.objects.get(pk=pk)
+        queryset = PostContent.objects.get(pk=pk, created_by=request.user)
         serializer = PostSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -39,15 +62,18 @@ class PostDetailView(APIView):
         return Response(serializer.errors)
 
     def delete(self, request, pk):
-        queryset = PostContent.objects.get(pk=pk)
+        queryset = PostContent.objects.get(pk=pk, created_by=request.user)
         queryset.delete()
         return Response(status=status.HTTP_200_OK)
 
 
+# ランダムな投稿取得
 class RandomPostView(APIView):
-    permission_classes = [IsAuthenticated]
+
+    permission_classes = [IsInGroup]
 
     def get(self, request):
-        queryset = PostContent.objects.order_by("?").first()
+        group_users = get_group_users(request.user)
+        queryset = PostContent.objects.filter(created_by__in=group_users).order_by("?").first()
         serializer = PostSerializer(queryset)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
