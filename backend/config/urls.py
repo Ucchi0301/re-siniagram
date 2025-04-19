c85a52ad7c944266c3ab29fd7f8883e8affbb741
LAPTOP-LN0I9EG8\ypom3 <ypom31@gmail.com>
2025-04-19 12:06:31 +0900

    ✨ 不要なURLパターンを削除し、APIのURLを整理

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls.conf import include
-from django.views.generic import TemplateView
 
 #swaggerの設定
 from rest_framework import permissions
@@ -25,55 +24,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("api/", include("api.urls")),
-    path("sinia/", TemplateView.as_view(template_name="sinia_home.html"), name="home"),
-    path(
-        "post_content/",
-        TemplateView.as_view(template_name="post_content.html"),
-        name="post_content",
-    ),
-    path(
-        "group_top/",
-        TemplateView.as_view(template_name="group_top.html"),
-        name="post",
-    ),
-    path(
-        "group_create/",
-        TemplateView.as_view(template_name="group_create.html"),
-        name="post",
-    ),
-    path(
-        "group_join/",
-        TemplateView.as_view(template_name="group_join.html"),
-        name="post",
-    ),
-    path(
-        "group_users/",
-        TemplateView.as_view(template_name="group_users.html"),
-        name="group_users",
-    ),
-    path(
-        "posts/",
-        TemplateView.as_view(template_name="posts.html"),
-        name="posts",
-    ),
-    path(
-        "post/<uuid:id>/",
-        TemplateView.as_view(template_name="post_detail.html"),
-        name="post",
-    ),
-    path(
-        "user/<uuid:id>/",
-        TemplateView.as_view(template_name="user_content.html"),
-        name="post",
-    ),
-    path(
-        "",
-        TemplateView.as_view(template_name="test_top.html"),
-        name="test",
-    ),
-    
+    path("api/", include("api.urls")),    
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
 ]
@@ -81,4 +32,3 @@ urlpatterns = [
 if settings.DEBUG:
     # 静的ファイルとメディアファイルの提供
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
